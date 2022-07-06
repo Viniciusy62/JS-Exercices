6be4dbac1a43e1a4d64d717b52523b9935173e5c
Vinicius de Andrade <viniciusy62@gmail.com>
2022-07-06 19:22:10 -0300

    updates

---
+++ ex009.js
@@ -0,0 +1,6 @@
+var velocidade = 60
+if(velocidade > 60){
+    console.log(`Sua velocidade é de ${velocidade} km/h. Você está acima da velocidade máxima permitida.`)
+}else{console.log('Dirija com cuidade e não ultrapasse a velocidade máxima da via.')}
+console.log('Use sempre o cinto de segurança.')
+
