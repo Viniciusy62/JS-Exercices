d9208718ea44e02dcfbe5c053c77e92961c2ea45
Vinicius de Andrade <viniciusy62@gmail.com>
2022-07-13 12:42:51 -0300

    updates

---
+++ ex010.js
@@ -0,0 +1,28 @@
+/* Exercício 1
+var idade = 16
+console.log(`Você tem ${idade} anos de idade`)
+if (idade < 16) {
+    console.log('Não vota')
+} else if (idade < 18 || idade >= 67) {
+        console.log('Voto opcional')
+    } else {
+        console.log('Voto obrigatório')
+    }
+*/
+
+/*Exercício 2*/
+
+//var agora = new Date()
+//var hora = agora.getHours() (Hora de onde está rodando o script)
+
+var hora = 0
+
+if (hora > 0 && hora < 12) {
+    console.log(`Bom dia! Agora são ${hora} horas`)
+} else if (hora <= 18 && hora != 0) {
+    console.log(`Boa tarde! Agora são ${hora} horas`)
+} else if (hora <= 24 || hora == 0){
+    console.log('Tenha uma boa noite!')
+} else {
+    console.log('Digite um valor válido de horas')
+}
