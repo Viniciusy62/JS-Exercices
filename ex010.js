a37a1e6cc9f0c16927918d81414e45e9b30ffab3
Vinicius de Andrade <viniciusy62@gmail.com>
2022-07-18 08:24:58 -0300

    Update ex010.js

--- ex010.js
+++ ex010.js
@@ -27,3 +27,38 @@ if (hora > 0 && hora < 12) {
     console.log('Digite um valor válido de horas')
 }
 */
+
+/*Exercício 3
+O dia através da função abaixo é mostra de 0 a 6, onde o 0 é domingo e o 6 é sábado
+*/
+
+var dia = new Date()
+var diaSemana = dia.getDay()
+
+switch (diaSemana) {
+    case 0:
+        console.log('Domingo')
+        break
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feira')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('[ERRO] Dia inválido!')
+        break
+}
+
